c78b97375242d2594fc08db941dfd34152916eb2
FabianSiffert1 <siffert@sipgate.de>
2025-07-15 14:09:22 +0200

    🐛 add nullCheck to CardList

--- src/Pages/Market/CardList/CardList.tsx
+++ src/Pages/Market/CardList/CardList.tsx
@@ -1,20 +1,21 @@
-import React, { ReactElement } from 'react'
-import { PokemonCard } from '../../../util/api/pokemonTGC/model/PokemonCard'
+import React, {ReactElement} from 'react'
+import {PokemonCard} from '../../../util/api/pokemonTGC/model/PokemonCard'
 import styles from './CardList.module.scss'
-import { Card } from './Components/Card/Card'
+import {Card} from './Components/Card/Card'
 
 export interface CardListProps {
-  cards: PokemonCard[]
+    cards: PokemonCard[]
 }
 
 export default function CardList(props: CardListProps) {
-  const cardArray: ReactElement<PokemonCard>[] = []
-
-  props.cards.forEach((card) => {
-    if (card.id != undefined) {
-      cardArray.push(<Card key={card.id} card={card} />)
+    const cardArray: ReactElement<PokemonCard>[] = []
+    if (props.cards != null) {
+        props.cards.forEach((card) => {
+            if (card.id != undefined) {
+                cardArray.push(<Card key={card.id} card={card}/>)
+            }
+        })
     }
-  })
 
-  return <div className={styles.cardList}>{cardArray}</div>
+    return <div className={styles.cardList}>{cardArray}</div>
 }
